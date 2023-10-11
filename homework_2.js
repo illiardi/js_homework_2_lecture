6f490c812ce625db77523726e5d6e7703be3cc56
Yashchenko Illia <illeeardi@gmail.com>
2023-10-11 16:02:30 +0300

    created&attemtp first objects

---
+++ homework_2.js
@@ -0,0 +1,10 @@
+// Тренарні оператори
+
+let numeric = 39;
+
+numeric % 2 ? console.log("Is not even number") : console.log("Is even number");
+
+// const IsEven = numeric % 2 ? "Not Even Number" : "Even Number";
+// console.log(IsEven); - додатково старався, а так те ж саме що і вище.
+
+// Об'єкти
